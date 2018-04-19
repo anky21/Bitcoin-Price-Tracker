905ae37d7552e08d9eee102d67cefb1d8a60906d
Anky An <anky25@gmail.com>
2018-04-20 03:21:08 +1000

    Set up pods

--- BitcoinTicker/ViewController.swift
+++ BitcoinTicker/ViewController.swift
@@ -43,7 +43,10 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         print(currencyArray[row])
+        finalURL = baseURL + currencyArray[row]
+        print(finalURL)
     }
+    
 
     
     
